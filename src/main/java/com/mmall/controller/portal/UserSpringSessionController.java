034077d8063bef0e0017d8b1f6b9a3b312b38e8f
ryan <isyangrui@foxmail.com>
2019-06-13 16:04:49 +0800

    fix bug

--- src/main/java/com/mmall/controller/portal/UserSpringSessionController.java
+++ src/main/java/com/mmall/controller/portal/UserSpringSessionController.java
@@ -79,7 +79,7 @@ public class UserSpringSessionController {
     @RequestMapping(value = "get_user_info.do", method = RequestMethod.GET)
     @ResponseBody // 自动序列化成 json
     public ServerResponse<User> getUserInfo(HttpSession session, HttpServletRequest httpServletRequest) {
-        User user = (User) session.getAttribute(Const.USERNAME);
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
 //        String loginToken = CookieUtil.readLoginToken(httpServletRequest);
 //        if (StringUtils.isEmpty(loginToken)) {
 //            return ServerResponse.createByErrorMessage("用户未登录，无法获取当前用户信息");
